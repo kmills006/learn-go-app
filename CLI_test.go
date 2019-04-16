a3f4b687d14051c7492c570d5e1d9a185b633255
Kristy Miller <kristy@poetic.io>
2019-04-16 15:18:00 -0500

    feat: add test checking if another user won

--- CLI_test.go
+++ CLI_test.go
@@ -6,12 +6,25 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-	in := strings.NewReader("Chris wins\n")
-	playerScore := &StubPlayerScore{}
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerScore := &StubPlayerScore{}
 
-	cli := &CLI{playerScore, in}
+		cli := &CLI{playerScore, in}
 
-	cli.PlayPoker()
+		cli.PlayPoker()
 
-	assertPlayerWin(t, playerScore, "Chris")
+		assertPlayerWin(t, playerScore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerScore := &StubPlayerScore{}
+
+		cli := &CLI{playerScore, in}
+
+		cli.PlayPoker()
+
+		assertPlayerWin(t, playerScore, "Cleo")
+	})
 }

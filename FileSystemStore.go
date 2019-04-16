f72e4a7fe95a6bee8b819fbfe30296fa460869cd
Kristy Miller <kristy@poetic.io>
2019-04-16 12:50:11 -0500

    feat: init store db file if it doesn't exist

--- FileSystemStore.go
+++ FileSystemStore.go
@@ -12,32 +12,39 @@ type FileSystemPlayerStore struct {
 	league   League
 }
 
-// NewFileSystemPlayerStore handles the initialization of the File System data store
-func NewFileSystemPlayerStore(database *os.File) (*FileSystemPlayerStore, error) {
-	database.Seek(0, 0)
+// initializePlayerDbFile initializes the store DB file
+func initializePlayerDbFile(file *os.File) error {
+	file.Seek(0, 0)
 
 	// file.Stat returns stats on our file. This lets us check the size of the file,
 	// if it's empty we Write an empty JSON array and Seek back to the start
 	// ready for the rest of the code.
-	info, err := database.Stat()
+	info, err := file.Stat()
 
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", database.Name(), err)
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
 	}
 
 	if info.Size() == 0 {
-		database.Write([]byte("[]"))
-		database.Seek(0, 0)
+		file.Write([]byte("[]"))
+		file.Seek(0, 0)
 	}
 
-	league, err := NewLeague(database)
+	return nil
+}
+
+// NewFileSystemPlayerStore handles the initialization of the File System data store
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initializePlayerDbFile(file)
+
+	league, err := NewLeague(file)
 
 	if err != nil {
-		return nil, fmt.Errorf("problem loading player store from file %s, %v", database.Name(), err)
+		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
 
 	return &FileSystemPlayerStore{
-		database: json.NewEncoder(&tape{database}),
+		database: json.NewEncoder(&tape{file}),
 		league:   league,
 	}, nil
 }

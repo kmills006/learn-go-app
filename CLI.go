167fecdb0cf1778b3e92955d14bf744324a6854a
Kristy Miller <kristy@poetic.io>
2019-04-16 15:14:47 -0500

    feat: add in value to cli initializer

--- CLI.go
+++ CLI.go
@@ -1,11 +1,14 @@
 package poker
 
+import "io"
+
 // CLI : TODO
 type CLI struct {
 	playerScore PlayerScore
+	in          io.Reader
 }
 
 // PlayPoker starts a new game of poker from the CLI
 func (c *CLI) PlayPoker() {
-	c.playerScore.RecordWin("Cleo")
+	c.playerScore.RecordWin("Chris")
 }
--- CLI_test.go
+++ CLI_test.go
@@ -1,15 +1,26 @@
 package poker
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
-	playerStore := &StubPlayerScore{}
+	in := strings.NewReader("Chris wins\n")
+	playerScore := &StubPlayerScore{}
 
-	cli := &CLI{playerStore}
+	cli := &CLI{playerScore, in}
 
 	cli.PlayPoker()
 
-	if len(playerStore.winCalls) != 1 {
+	if len(playerScore.winCalls) != 1 {
 		t.Fatal("expected a win call but didn't get any")
+	}
+
+	got := playerScore.winCalls[0]
+	want := "Chris"
+
+	if got != want {
+		t.Errorf("didn't record correct winner, got %s, want %s", got, want)
 	}
 }

22362047fe52c20fa91ca2a73a670e918214887b
Kristy Miller <kristy@poetic.io>
2019-04-16 10:50:06 -0500

    feat: create type league and find method

--- league.go
+++ league.go
@@ -6,6 +6,20 @@ import (
 	"io"
 )
 
+// League represents a list of players
+type League []Player
+
+// Find will return an individual player by their name
+func (l League) Find(name string) *Player {
+	for i, p := range l {
+		if p.Name == name {
+			return &l[i]
+		}
+	}
+
+	return nil
+}
+
 // NewLeague returns the decoded league from the JSON data store
 func NewLeague(rdr io.Reader) ([]Player, error) {
 	var league []Player

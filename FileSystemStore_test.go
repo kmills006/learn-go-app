01e3f47f0f03052ec11f8e500fefabd57342824e
Kristy Miller <kristy@poetic.io>
2019-04-16 10:56:28 -0500

    feat: add test to store wins for new players

--- FileSystemStore_test.go
+++ FileSystemStore_test.go
@@ -57,6 +57,23 @@ func TestFileSystemStore(t *testing.T) {
 
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("store wins for new players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+		{"Name": "Cleo", "Wins": 10},
+		{"Name": "Chris", "Wins": 23}]`)
+
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Pepper")
+
+		got := store.GetPlayerScore("Pepper")
+		want := 1
+
+		assertScoreEquals(t, got, want)
+	})
 }
 
 // createTempFile creates a temporary file with some data inside of it

f3062512a8ed45dda7f7c976a985e73dd643ab20
Kristy Miller <kristy@poetic.io>
2019-04-16 08:13:56 -0500

    feat: add integration tests

--- main.go
+++ main.go
@@ -5,19 +5,8 @@ import (
 	"net/http"
 )
 
-// InMemoryPlayerStore : TODO
-type InMemoryPlayerStore struct{}
-
-// GetPlayerScore : TODO
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-// RecordWin : TODO
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
